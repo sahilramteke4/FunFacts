3e0a662e450270f5afa0ec7cb326f62535f448aa
sahilramteke4 <59560511+sahilramteke4@users.noreply.github.com>
2020-08-21 00:58:06 +0530

    Updates

---
+++ precache-manifest.190dbc5072b4a65ea6d9b6b4d9199184.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e375f70b51d1a5cf704c4c258ab78227",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fc1ffa466026091e973b",
+    "url": "/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "5f0b55f47bf308b5b340",
+    "url": "/static/css/main.a29cb8f7.chunk.css"
+  },
+  {
+    "revision": "fc1ffa466026091e973b",
+    "url": "/static/js/2.c6339534.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c6339534.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f0b55f47bf308b5b340",
+    "url": "/static/js/main.ddb2b1e8.chunk.js"
+  },
+  {
+    "revision": "2019bf1f616f83d24369",
+    "url": "/static/js/runtime-main.4f31e145.js"
+  },
+  {
+    "revision": "6fe8a60264d2df0047afe01924397446",
+    "url": "/static/media/blu-plain.6fe8a602.ttf"
+  },
+  {
+    "revision": "263278534cf850b45c7aadfab31abe5a",
+    "url": "/static/media/chucknorrisfont.26327853.ttf"
+  },
+  {
+    "revision": "94056d87e192cfc0a9043700baf178dc",
+    "url": "/static/media/pokedexfont.94056d87.ttf"
+  },
+  {
+    "revision": "831e1f4da157d96bc996f8c9f5f1e578",
+    "url": "/static/media/pokemondetailsfont.831e1f4d.ttf"
+  }
+]);
